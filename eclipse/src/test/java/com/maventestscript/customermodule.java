f55cee0706fe4c14981dba41bdffde5271aa8e11
radha Krishna <radha Krishna@DESKTOP-MC0BH2A>
2024-01-05 13:21:35 +0530

    first commit

---
+++ eclipse/src/test/java/com/maventestscript/customermodule.java
@@ -0,0 +1,14 @@
+package com.maventestscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class customermodule {
+
+	@Test
+	
+	public void createcustomer() {
+		Reporter.log("createcustomer",true);
+		}
+	
+}
